990c0c440efa329a55f37de0f1a9623e07032e16
Everton Ferreira <eferreira@viceri.com.br>
2020-01-23 10:44:56 -0300

    kata#4 end

--- BowlingGame/#4Kata/BowlingGame/src/main/java/BowlingGame/Game.java
+++ BowlingGame/#4Kata/BowlingGame/src/main/java/BowlingGame/Game.java
@@ -1,9 +1,5 @@
 package BowlingGame;
 
-/**
- * Hello world!
- *
- */
 public class Game {
     private final int[] rolls = new int[21];
     private int current = 0;
@@ -13,18 +9,37 @@ public class Game {
     }
 
     public int score() {
-        int score = 0;
+        int result = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] + rolls[frameIndex + 1] == 10) {
-                score += 10 + rolls[frameIndex + 2];
-                frameIndex+=2;
-            }
-            else{
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
-                frameIndex+=2;
+
+            if (isStrike(frameIndex)) {
+                result += strikeScore(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                result += spareScore(frameIndex);
+                frameIndex += 2;
+            } else {
+                result += rolls[frameIndex] + rolls[frameIndex + 1];
+                frameIndex += 2;
             }
         }
-        return score;
+        return result;
+    }
+
+    private int strikeScore(int frameIndex) {
+        return 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private int spareScore(int frameIndex) {
+        return 10 + rolls[frameIndex + 2];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 }
--- BowlingGame/#4Kata/BowlingGame/src/test/java/BowlingGame/GameTest.java
+++ BowlingGame/#4Kata/BowlingGame/src/test/java/BowlingGame/GameTest.java
@@ -4,38 +4,62 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class GameTest 
-{
+public class GameTest {
     private Game game = new Game();
+
     @Test
-    public void gutterGame()
-    {
+    public void gutterGame() {
         rollMany(20, 0);
         assertEquals(0, game.score());
     }
 
-    
     @Test
-    public void allOnesGame()
-    {
+    public void allOnesGame() {
         rollMany(20, 1);
         assertEquals(20, game.score());
     }
 
     @Test
-    public void spareGame() {
-        rollMany(2, 5);
+    public void oneSpareGame() {
+        spare();
         game.roll(3);
         rollMany(17, 0);
         assertEquals(16, game.score());
     }
 
+    @Test
+    public void allSpareGame() {
+        rollMany(21, 5);
+        assertEquals(150, game.score());
+    }
+
+    @Test
+    public void oneStrikeGame() {
+        strike();
+        game.roll(4);
+        game.roll(4);
+        rollMany(17, 0);
+        assertEquals(26, game.score());
+    }
+
+    @Test
+    public void perfectGame() {
+        rollMany(20, 10);
+        assertEquals(300, game.score());
+    }
+
+    private void strike() {
+        game.roll(10);
+    }
+
+    private void spare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
     private void rollMany(int rolls, int pins) {
         for (int i = 0; i < rolls; i++) {
             game.roll(pins);
-        }    
+        }
     }
 }
