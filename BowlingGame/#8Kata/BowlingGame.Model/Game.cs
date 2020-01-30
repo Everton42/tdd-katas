ff27b805131e7e8f89a2e883609bc8f568f538cc
Everton Ferreira <evertonferreira4596@gmail.com>
2020-01-30 20:18:26 -0300

    kata#8

---
+++ BowlingGame/#8Kata/BowlingGame.Model/Game.cs
@@ -0,0 +1,66 @@
+﻿namespace BowlingGame.Model
+{
+    public class Game
+    {
+        private int[] Rolls = new int[21];
+        private int Current = 0;
+
+        public int Score { get; set; }
+        public void roll(int pins)
+        {
+            Rolls[Current++] += pins;
+        }
+
+        public int score()
+        {
+            int result = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    result += StrikeScore(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    result += SpareScore(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    result += CommumScore(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+
+            return result;
+        }
+
+        private int CommumScore(int frameIndex)
+        {
+            return Rolls[frameIndex] + Rolls[frameIndex + 1];
+        }
+
+        private int SpareScore(int frameIndex)
+        {
+            return 10 + Rolls[frameIndex + 2];
+        }
+
+        private int StrikeScore(int frameIndex)
+        {
+            return 10 + Rolls[frameIndex + 1] + Rolls[frameIndex + 2];
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return Rolls[frameIndex] + Rolls[frameIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return Rolls[frameIndex] == 10;
+        }
+    }
+}
---
+++ BowlingGame/#8Kata/BowlingGame.Test/GameTest.cs
@@ -0,0 +1,89 @@
+using BowlingGame.Model;
+using Xunit;
+
+namespace BowlingGame.Test
+{
+    public class GameTest
+    {
+        public Game Game { get; set; }
+        public GameTest()
+        {
+            Game = new Game();
+        }
+
+        [Fact]
+        public void GivenGameRollWhenNoPinsDownThenReturnScoreZero()
+        {
+            int pins = 0;
+            int rolls = 20;
+
+            RollMany(rolls, pins);
+
+            int result = Game.score();
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void GivenGameRollWhenAllRollsHitOnePinThenReturnScoreTwenty()
+        {
+            int pins = 1;
+            int rolls = 20;
+
+            RollMany(rolls, pins);
+
+            int result = Game.score();
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void GivenGameRollWhenHitTenPinsInTwoRollThenReturnScoreWithOneBonusRoll()
+        {
+            SpareRoll();
+            Game.roll(4);
+            RollMany(17, 0);
+
+            int result = Game.score();
+            Assert.Equal(18, result);
+        }
+
+        [Fact]
+        public void GivenGameRollWhenHitTenPinsInOneRollThenReturnScoreWithTwoBonusRoll()
+        {
+            StrikeRoll();
+            Game.roll(4);
+            Game.roll(4);
+            RollMany(17, 0);
+
+            int result = Game.score();
+            Assert.Equal(26, result);
+        }
+
+        [Fact]
+        public void GivenGameRollWhenHitTenPinsInAllRollsThenReturnMaximumScore()
+        {
+            RollMany(20, 10);
+
+            int result = Game.score();
+            Assert.Equal(300, result);
+        }
+
+        private void StrikeRoll()
+        {
+            Game.roll(10);
+        }
+
+        private void SpareRoll()
+        {
+            Game.roll(5);
+            Game.roll(5);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                Game.roll(pins);
+            }
+        }
+    }
+}
